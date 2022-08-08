206cd4053b799bbb5112b6a68bb82f7dfa9d8127
Vinh Pham <vinhpt2@vng.com.vn>
2022-08-08 17:25:07 +0700

    Init source

---
+++ js/scripts.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+
+  //Scroll animation  
+  AOS.init({offset:0, duration:1000, delay:200});
+
+  //Scrollbar
+  $(".scrollbar").mCustomScrollbar();
+
+  //Sidebar
+  $('.action').click(function(){
+    $('.sidebar').toggleClass('show');
+  });
+
+  //Close youtube
+  $('.modal-video').on('hidden.bs.modal', function (e) {
+    var $this = $(this).find('video'),
+            tempSrc = $this.attr('src');
+    $this.attr('src', "");
+    $this.attr('src', tempSrc);
+  });
+
+  //Slider 
+  if ( $( ".mySwiper" ).length ){
+    var swiper = new Swiper(".mySwiper", {
+      slidesPerView: 2,
+      spaceBetween: 50,
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 50,
+        },
+        768: {
+          slidesPerView: 1,
+          spaceBetween: 50,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 50,
+        },
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
+
+  //Button hide
+  $('.btn-toogle').click(function(){
+    $(this).parent().toggleClass('hide');
+    $(this).parent().parent().find('.content').toggleClass('hide');
+  })
+
+});
